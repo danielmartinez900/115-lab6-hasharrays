74b2ae1f9399f62ee3b8d1be3187de85e3ad3733
Daniel Martinez <danielmartinez900@mail.fresnostate.edu>
2021-10-27 11:43:40 -0700

    updated source code. Hash class included

--- rjhash.cpp
+++ rjhash.cpp
@@ -1,18 +1,91 @@
-#include <iostream>
-#include <fstream>
-#include <cstdlib>
-#include <vector>
-#include "hash-class-header.h"
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
 
+const int HASHSIZE = 4001;
+struct entry
+{
+	string word;
+	int freq;
+};
+
+class Hash
+{
+public:
+	entry hasharray[HASHSIZE];
+	int updates[HASHSIZE];
+	int cost[HASHSIZE];
+	int size, probe, num = 0;
+	Hash() { hasharray; }
+	int getsize()
+	{
+		return size;
+	}
+	int getprobes()
+	{
+		return probe;
+	}
+	int hashkey(string str)
+	{
+		std::hash<std::string>()(str);    //built in hash function
+	}
+	void update(string str)
+	{
+		int key = hashkey(str);   //find hashkey of the input string
+		size = 0;
+		probe = 0;				  //initalize probe to 0
+		if (hasharray[key].freq == 0)   //not present
+		{
+			hasharray[key].freq++;
+			hasharray[key].word = str;
+			size++; probe++; num++;
+		}
+		else if (hasharray[key].word == str)     //word present
+		{
+			hasharray[key].freq++;
+			probe++;
+		}
+		else 
+		{
+			while (hasharray[key].freq != 0 && hasharray[key].word != key)    //linear probing
+			{
+				key++;
+				probe++;
+			}
+			if (hasharray[key].word == str) {
+				hasharray[key].freq++;
+				probe++;
+			}
+			else
+			{
+				hasharray[key].word = str;
+				size++; num++; probe++;
+			}
+		}
+		updates[size] = num;  //update the 2 stats arrays
+		cost[size] = probe;
+
+	}
+	
+};
 
 int main()
 {
-    Hash hash1;
-    Hash hash2;
-    Hash hash3;
+	Hash hash1;
+	string randj;
+	string word, filename;
+	fstream myfile;
+	filename = "RomeoAndJuliet.txt";
+	myfile.open(filename.c_str());
+	while (myfile >> word)      //read the file word by word
+	{
+		hash1.update(word);   //update the hasharray with the word that was just read
+	}
 
-    cout<<"new changes"<<endl;
-    cout<<"header now in"<<endl;
+	cout << "number of unique words: " << hash1.updates << endl;
+	cout << "cost: " << hash1.cost;
 
-    return 0;
+
+	return 0;
 }
